2a0545fc622e79af1290d6b4ceb621c3e79e46b7
Piotr Kedziora <piotrkedziorapl@gmail.com>
2018-05-30 13:01:44 +0200

    Fix bugs

--- routes/admins/typy.js
+++ routes/admins/typy.js
@@ -9,13 +9,13 @@ router.get('/', (req, res)=>{
                 if (err) throw err;
                 else {
                     const mecze = result[0].mecze;
-                    console.log(mecze);
                     connect.query(`SELECT u.id, u.login, COUNT(*) as typy FROM types t, users u WHERE typed=1 AND u.id=t.id_user GROUP BY login ORDER BY login`, (err, result)=>{
                         if (err) throw err;
                         const wytypowane = result;
                         res.render('admin/typy', {
                             wytypowane,
-                            mecze: mecze
+                            mecze: mecze,
+                            layout: 'admin_layout'
                         });
                         
                     });
